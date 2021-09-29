df5f9ed2c146739b6c6da60e30dd5b45927dc177
psychoactiv <pritam54majumdar@gmail.com>
2021-09-28 19:39:10 -0700

    initial

--- script.js
+++ script.js
@@ -8,16 +8,24 @@ function newFunc(initialPrice, amountOfStocks, currentAmount) {
   var initial = Number(initialPrice.value);
   var noOfStocks = Number(amountOfStocks.value);
   var current = Number(currentAmount.value);
-  if (initial < current) {
-    var profit = (current - initial) * noOfStocks;
-    var profitPercntage = (profit / initial) * 100;
-    output.innerText = `congrats you made a profit of ₹${profit} and a profit percentage of ${profitPercntage}%`;
-  } else if (initial > current) {
-    var loss = (initial - current) * noOfStocks;
-    var lossPercntage = (loss / initial) * 100;
-    output.innerText = `sorry you got a loss of ₹${loss} and a loss percentage of ${lossPercntage}%`;
+  if (initial === 0 && noOfStocks === 0 && current === 0) {
+    alert("Please fill out all Fields");
   } else {
-    output.innerText = `No pain no gain , No gain no pain`;
+    if (initial < current) {
+      var profit = (current - initial) * noOfStocks;
+      var profitPercntage = (profit / initial) * 100;
+      output.innerText = `congrats you made a profit of ₹${profit} and a profit percentage of ${profitPercntage.toFixed(
+        2
+      )}%`;
+    } else if (initial > current) {
+      var loss = (initial - current) * noOfStocks;
+      var lossPercntage = (loss / initial) * 100;
+      output.innerText = `sorry you got a loss of ₹${loss} and a loss percentage of ${lossPercntage.toFixed(
+        2
+      )}%`;
+    } else {
+      output.innerText = `No pain no gain , No gain no pain`;
+    }
   }
 }
 
